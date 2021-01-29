1ece5d84808209b24c072865629f68b0fb6e441d
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-01-28 20:04:25 -0500

    Added types.d.ts & result.d.ts to lib/types.

---
+++ lib/types/types.d.ts
@@ -0,0 +1,14 @@
+export interface IConnect {
+    recipient_node_peer_id: string;
+    recipient_user_peer_id: string;
+    sender_node_peer_id: string;
+    sender_user_peer_id: string;
+}
+export interface ILogin {
+    chainNodeType: string;
+    htlcFeeRate: number;
+    htlcMaxFee: number;
+    nodeAddress: string;
+    nodePeerId: string;
+    userPeerId: string;
+}
