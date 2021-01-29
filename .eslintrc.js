15e5ad40b348426ef36d13ef1c616dc31f8901a8
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-01-28 19:33:58 -0500

    Created result.ts & types.ts. Added node_modules to .eslintignore. Updated .eslintrc. Updated lint script in package.json. Updated yarn.lock.

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,9 @@ module.exports = {
     semi: 2,
     "no-undef": 0,
     "require-atomic-updates": 0,
-    "no-async-promise-executor": 0
+    "no-async-promise-executor": 0,
+    "multiline-comment-style": 0,
+    "no-unused-vars": 0
   },
   globals: {
     fetch: false
