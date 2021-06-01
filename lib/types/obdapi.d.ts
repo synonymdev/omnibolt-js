18f6c6132591201d9e44b02e634cb959b575a25e
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-06-01 14:37:33 -0400

    Added onChannelCloseAttempt to connect method in obdapi.ts. Added ICloseChannel type to src/types.ts.

--- lib/types/obdapi.d.ts
+++ lib/types/obdapi.d.ts
@@ -1,6 +1,6 @@
 import { MessageType, Message, P2PPeer, BtcFundingInfo, FundingBtcCreated, FundingBtcSigned, OmniFundingAssetInfo, OmniSendAssetInfo, OpenChannelInfo, AcceptChannelInfo, AssetFundingCreatedInfo, AssetFundingSignedInfo, SignedInfo100100, SignedInfo100101, SignedInfo100102, SignedInfo100103, SignedInfo100104, SignedInfo100105, SignedInfo100106, SignedInfo100110, SignedInfo100111, SignedInfo100112, SignedInfo100113, SignedInfo100114, SignedInfo100360, SignedInfo100361, SignedInfo100362, SignedInfo100363, SignedInfo100364, SignedInfo101035, SignedInfo101134, CommitmentTx, CommitmentTxSigned, InvoiceInfo, HTLCFindPathInfo, addHTLCInfo, HtlcSignedInfo, ForwardRInfo, SignRInfo, CloseHtlcTxInfo, CloseHtlcTxInfoSigned, IssueFixedAmountInfo, IssueManagedAmoutInfo, OmniSendGrant, OmniSendRevoke, CloseChannelSign, AtomicSwapAccepted, AtomicSwapRequest } from "./pojo";
 import { Result } from "./result";
-import { IAcceptChannel, IConnect, IGetMyChannels, ILogin, IFundingBitcoin, IBitcoinFundingCreated, ISendSignedHex100341, TOnBitcoinFundingCreated, TOnChannelOpenAttempt, IBitcoinFundingSigned, TOnAssetFundingCreated, IAssetFundingSigned, TSendSignedHex101035, TOnCommitmentTransactionCreated, ICommitmentTransactionAcceptedResponse, ISendSignedHex100361Response, TOnAcceptChannel, TOn110353, ICommitmentTransactionCreated, TOn110352, ISendSignedHex100364Response, ISendSignedHex100362Response, ISendSignedHex100363Response, IGetProperty } from "./types";
+import { IAcceptChannel, IConnect, IGetMyChannels, ILogin, IFundingBitcoin, IBitcoinFundingCreated, ISendSignedHex100341, TOnBitcoinFundingCreated, TOnChannelOpenAttempt, IBitcoinFundingSigned, TOnAssetFundingCreated, IAssetFundingSigned, TSendSignedHex101035, TOnCommitmentTransactionCreated, ICommitmentTransactionAcceptedResponse, ISendSignedHex100361Response, TOnAcceptChannel, TOn110353, ICommitmentTransactionCreated, TOn110352, ISendSignedHex100364Response, ISendSignedHex100362Response, ISendSignedHex100363Response, IGetProperty, ICloseChannel } from "./types";
 export default class ObdApi {
     constructor({ url }?: {
         url?: string | undefined;
@@ -15,6 +15,7 @@ export default class ObdApi {
     onOpen: ((data: any) => any) | undefined;
     onMessage: Function | undefined;
     onChannelOpenAttempt: ((data: TOnChannelOpenAttempt) => any) | undefined;
+    onChannelCloseAttempt: ((data: any) => any) | undefined;
     onAcceptChannel: ((data: TOnAcceptChannel) => any) | undefined;
     onBitcoinFundingCreated: ((data: TOnBitcoinFundingCreated) => any) | undefined;
     onAssetFundingCreated: ((data: TOnAssetFundingCreated) => any) | undefined;
@@ -27,11 +28,12 @@ export default class ObdApi {
         nodePeerId: string;
         userPeerId: string;
     };
-    connect({ url, onOpen, onMessage, onChannelOpenAttempt, onAcceptChannel, onBitcoinFundingCreated, onAssetFundingCreated, onCommitmentTransactionCreated, on110353, on110352, onChannelClose, onError, onClose, onAddHTLC, onForwardR, onSignR, onCloseHTLC }: {
+    connect({ url, onOpen, onMessage, onChannelOpenAttempt, onChannelCloseAttempt, onAcceptChannel, onBitcoinFundingCreated, onAssetFundingCreated, onCommitmentTransactionCreated, on110353, on110352, onChannelClose, onError, onClose, onAddHTLC, onForwardR, onSignR, onCloseHTLC }: {
         url: string | undefined;
         onOpen: (data: any) => any;
         onMessage: (data: any) => any;
         onChannelOpenAttempt: (data: TOnChannelOpenAttempt) => any;
+        onChannelCloseAttempt: (data: any) => any;
         onAcceptChannel: (data: TOnAcceptChannel) => any;
         onBitcoinFundingCreated: (data: TOnBitcoinFundingCreated) => any;
         onAssetFundingCreated: (data: TOnAssetFundingCreated) => any;
@@ -173,7 +175,7 @@ export default class ObdApi {
     onSendSomeCommitmentById(jsonData: any): void;
     getAllRevockableDeliveryTransactions(channel_id: string): Promise<unknown>;
     onGetAllRevockableDeliveryTransactions(jsonData: any): void;
-    closeChannel(recipient_node_peer_id: string, recipient_user_peer_id: string, channel_id: string): Promise<unknown>;
+    closeChannel(recipient_node_peer_id: string, recipient_user_peer_id: string, channel_id: string): Promise<Result<ICloseChannel>>;
     onCloseChannel(jsonData: any): void;
     closeChannelSigned(recipient_node_peer_id: string, recipient_user_peer_id: string, info: CloseChannelSign): Promise<unknown>;
     onCloseChannelSigned(jsonData: any): void;
