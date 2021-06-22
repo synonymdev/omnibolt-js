45484bc5fa5a1e7bf3eca8b0cd7344d4d05b345b
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-06-21 22:38:34 -0400

    Updated .eslintrc.js, .prettierrc.js & tsconfig.json.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,37 @@
 module.exports = {
-  parser: "babel-eslint",
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
-    commonjs: true
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  extends: ["eslint:recommended"],
-  rules: {
-    "no-console": 0,
-    "no-empty": ["error", { allowEmptyCatch: true }],
-    "no-buffer-constructor": 0,
-    "no-case-declarations": 0,
-    "no-useless-escape": 0,
-    semi: 2,
-    "no-undef": 0,
-    "require-atomic-updates": 0,
-    "no-async-promise-executor": 0,
-    "multiline-comment-style": 0,
-    "no-unused-vars": 0
-  },
-  globals: {
-    fetch: false
-  }
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	globals: {
+		localStorage: false,
+	},
+	rules: {
+		semi: 'off',
+		'@typescript-eslint/semi': ['error'],
+		'no-shadow': 'off',
+		'@typescript-eslint/no-shadow': 'error',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'no-console': 0,
+		'no-empty': ['error', { allowEmptyCatch: true }],
+		'no-buffer-constructor': 0,
+		'no-case-declarations': 0,
+		'no-useless-escape': 0,
+		indent: [
+			2,
+			'tab',
+			{ SwitchCase: 1, ignoredNodes: ['ConditionalExpression'] },
+		],
+		'object-curly-spacing': [
+			'error',
+			'always',
+			{
+				objectsInObjects: true,
+			},
+		],
+		'no-undef': 0,
+		'require-atomic-updates': 0,
+		'no-async-promise-executor': 0,
+		'brace-style': [2, '1tbs', { allowSingleLine: true }],
+		'@typescript-eslint/explicit-function-return-type': 'warn',
+	},
 };
