cb207b19c7991947b4b9d3e06de19b63ece9582a
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-06-21 22:32:58 -0400

    Added shapes.ts. Added utils/index.ts. & utils/networks.ts.

---
+++ src/shapes.ts
@@ -0,0 +1,35 @@
+import { IChannelSigningData, IData } from './types';
+
+export const defaultDataShape: IData = {
+	nextAddressIndex: {
+		index: 0,
+		path: '',
+		address: '',
+		scriptHash: '',
+		publicKey: '',
+	},
+	signingData: {},
+	checkpoints: {},
+};
+
+export const addressContent = {
+	index: 0,
+	path: '',
+	address: '',
+	scriptHash: '',
+	publicKey: '',
+};
+
+export const channelSigningData: IChannelSigningData = {
+	fundingAddress: { ...addressContent },
+	addressIndex: { ...addressContent },
+	last_temp_address: { ...addressContent },
+	rsmc_temp_address: { ...addressContent },
+	htlc_temp_address: { ...addressContent },
+	htlc_temp_address_for_he1b: { ...addressContent },
+	kTbSignedHex: '',
+	funding_txid: '',
+	kTempPrivKey: '',
+	kTbSignedHexCR110351: '',
+	kTbSignedHexRR110351: '',
+};
