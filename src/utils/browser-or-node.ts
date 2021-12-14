1e90a4730f5d46d15a54e9a2032a2a7f353f9409
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-12-13 23:17:02 -0500

    Updated websocket condition in connect method in obdapi.ts. Added try/catch to browser-or-node.ts methods. Removed prepublish from scripts in package.json.

--- src/utils/browser-or-node.ts
+++ src/utils/browser-or-node.ts
@@ -1,25 +1,49 @@
 /* global window self */
 
-const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+const isBrowser = (): boolean => {
+	try {
+		return typeof window !== 'undefined' && typeof window.document !== 'undefined';
+	} catch {
+		return false;
+	}
+}
 
 /* eslint-disable no-restricted-globals */
-const isWebWorker = typeof self === 'object'
-	&& self.constructor
-	&& self.constructor.name === 'DedicatedWorkerGlobalScope';
+const isWebWorker = (): boolean => {
+	try {
+		return typeof self === 'object'
+		&& self.constructor
+		&& self.constructor.name === 'DedicatedWorkerGlobalScope';
+	} catch {
+		return false;
+	}
+}
 /* eslint-enable no-restricted-globals */
 
-const isNode = typeof process !== 'undefined'
-	&& process.versions != null
-	&& process.versions.node != null;
+const isNode = (): boolean => {
+	try {
+		return typeof process !== 'undefined'
+		&& process.versions != null
+		&& process.versions.node != null;
+	} catch {
+		return false;
+	}
+}
 
 /**
  * @see https://github.com/jsdom/jsdom/releases/tag/12.0.0
  * @see https://github.com/jsdom/jsdom/issues/1537
  */
 /* eslint-disable no-undef */
-const isJsDom = (): boolean => (typeof window !== 'undefined' && window.name === 'nodejs')
-	|| navigator.userAgent.includes('Node.js')
-	|| navigator.userAgent.includes('jsdom');
+const isJsDom = (): boolean => {
+	try {
+		return (typeof window !== 'undefined' && window.name === 'nodejs')
+			|| navigator.userAgent.includes('Node.js')
+			|| navigator.userAgent.includes('jsdom');
+	} catch {
+		return false;
+	}
+}
 
 export {
 	isBrowser, isWebWorker, isNode, isJsDom
